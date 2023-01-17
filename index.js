3a26747af50d4ec7954fd46cdf8c574d97a3b0f4
Olga Pavlenko <pavolen@mail.ru>
2023-01-17 12:47:50 +0300

    work

---
+++ index.js
@@ -0,0 +1,19 @@
+function addition() {
+    var a = parseInt(document.getElementById("a").value);
+    var b = parseInt(document.getElementById("b").value);
+    var d = parseInt(document.getElementById("d").value);
+    if (isNaN(a) == true) a = 0;
+    if (isNaN(b) == true) b = 0;
+    if (isNaN(d) == true) d = 0;
+
+    var c = a + b + d;
+
+    document.getElementById("result").innerHTML = c;
+  }
+   function percent(number){
+    var percent="30";
+    var number="15121";
+    var number_percent=number/100*percent;
+    var x = Number(number) - Number(number_percent);
+    document.getElementById("finalresult").innerHTML = x;
+   }
